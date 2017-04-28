5faa85512b4acf0451c68fa81609ed1c4c06fc9d
shensu <shensu@omg.local>
2017-04-28 15:11:31 +0800

    提交代码

--- +/codeObfuscation.h
+++ +/codeObfuscation.h
@@ -1,14 +1,14 @@
 #ifndef Demo_codeObfuscation_h
 #define Demo_codeObfuscation_h
-//confuse string at Fri Apr 28 14:54:58 CST 2017
-#define addActionView XRhpdhvSkLLSiKAz
-#define pinJieStr nsCQvhEHckxaAFnw
-#define setJinShaVc wjHbNYQhXDcaVSOV
-#define savadata IjnqpRkYRcntptgS
-#define submitAction GBrGbwxLcPyxiIpn
-#define setStaticGuidePage hBrtOyqyTbEzVkdc
-#define addsomething fCHhxskYRWZJZRdl
-#define setBannar wEuTiLhUectOTKUk
-#define loadNewItems NFGWAHtkaPgNXVSZ
-#define shiwan cLcHxHFSHIHiadRU
+//confuse string at Fri Apr 28 15:10:18 CST 2017
+#define addActionView IikpqkNWqAraIhJS
+#define pinJieStr ssKfQMEHtaUkvDmA
+#define setJinShaVc UWwqShWGyQvmkbQq
+#define savadata lshfCKkxCfAjJrry
+#define submitAction JzucDdyGUHfXjxOB
+#define setStaticGuidePage aCtckFJXDRtuyBkO
+#define addsomething zDsFVfxjYDkXakNo
+#define setBannar PyYEOhHTobXqRkJD
+#define loadNewItems iFOtBPzHbQzRrdwh
+#define shiwan RziHJBCUJyTphsYg
 #endif

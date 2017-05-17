7f43fc2874831abd6301c2245107fcf2ecf851d9
shensu <shensu@omg.local>
2017-05-17 11:23:41 +0800

    提交代码

--- shensu/codeObfuscation.h
+++ shensu/codeObfuscation.h
@@ -1,32 +1,32 @@
 #ifndef Demo_codeObfuscation_h
 #define Demo_codeObfuscation_h
-//confuse string at Tue May 16 10:40:12 CST 2017
-#define addActionView hEigadBgcMlOtsZa
-#define pinJieStr EkJXpGhEfkaEBApe
-#define setJinShaVc cKYlYZXBviMyGgsS
-#define savadata GzGPsKBkJfnJthDS
-#define setStaticGuidePage xJjCNGhGnHoxOuST
-#define addsomething BoRNGWZyTQIUjOfR
-#define setBannar SzhzygnsnympNfnK
-#define loadNewItems FzstrmMGggBUcczU
-#define shiwan RsWgRkmxUJacsrad
-#define getData iWcHJCesbVwDbMse
-#define getqishudata QFbfqnvANbUYPogV
-#define getdata UuweAZZkHGJfCVcJ
-#define setAppDelegateModel liSWKWlXCPDDHacg
-#define setUpMoney RDUfOabBaoiajSaV
-#define getgpcData vRJFiEhQuLcROZIO
-#define submitAction bCncSHxvggNqRnaR
-#define loadNewImtes jepcFEEJECTAjCJF
-#define addBtn iYHpAONZODSuaWAq
-#define setUI hXPakVGKtApbxxKH
-#define addTS yBozTZOUMcdmAIxj
-#define popTZ eYpndmwmStZUJEdz
-#define btnClick UfDXYvfpUxkmAnaR
-#define leftClick PpwEKKZSGTyKVDTW
-#define rightClick hyFWvkbGuBvDdyTm
-#define createToolBar FropLGtAOYLjqMfu
-#define addyindaoyue oYbzOpjUHvJUbtje
-#define selectedAll JIPfbNNgRvRspfoZ
-#define xiazhuBtnClick gUbTCsSSToyxWLni
+//confuse string at Wed May 17 11:22:03 CST 2017
+#define addActionView cHrTnntIyrXesKXA
+#define pinJieStr eIHQKAStKcSPXRsR
+#define setJinShaVc OdjPFPgnzXduphEY
+#define savadata eTfjPNZeWeadxMYO
+#define setStaticGuidePage KHkUjZAitKAjOHEL
+#define addsomething EEhugIuMhBeZuAba
+#define setBannar DaRdYtigdZjkfQKn
+#define loadNewItems mvWWHyUrPLSQRrNP
+#define shiwan rJZDSxozOxuCdMnY
+#define getData EgnCWubIStSQiumC
+#define getqishudata BDgQoTkLArrcDSMm
+#define getdata nKrOzHcnFMUjDykO
+#define setAppDelegateModel VIqzQoDrDAPXKUkM
+#define setUpMoney aQPDdsvqZtijmYan
+#define getgpcData MECudFzgauiqJXXz
+#define submitAction fPZtSUFOpMgEvKNq
+#define loadNewImtes fwfQYEQhwTTXIQTj
+#define addBtn WKOzuKrjzYeYnxpJ
+#define setUI viBveOWNNWlMJTNk
+#define addTS HqeOaRrBlhvZSYPo
+#define popTZ gbZjTjjNsyXfOxRR
+#define btnClick ELRmyTgQZnhUuZLt
+#define leftClick ekbYIIdPsyGluKkw
+#define rightClick mHulKJYtuQBnaKya
+#define createToolBar EVgjemXadIFRkTOF
+#define addyindaoyue pbJiKiyvKEHbQhUN
+#define selectedAll eYVJBvtvFGZcYGzs
+#define xiazhuBtnClick TLcfwdKiBPXmLfra
 #endif

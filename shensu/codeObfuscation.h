5637834d32c07954a45dfb1c1b7ce33881dddfe0
shensu <shensu@omg.local>
2017-05-31 12:27:28 +0800

    重庆时时彩

--- shensu/codeObfuscation.h
+++ shensu/codeObfuscation.h
@@ -1,33 +1,33 @@
 #ifndef Demo_codeObfuscation_h
 #define Demo_codeObfuscation_h
-//confuse string at Wed May 31 11:41:48 CST 2017
-#define addActionView LGOBXKfdZSrggFBO
-#define pinJieStr GvUYEsiUgrqYETyQ
-#define setJinShaVc eGexcbonGfXnrvsX
-#define savadata kfcbispROrrsAKfw
-#define setStaticGuidePage pokvbhoTcwSetjwL
-#define addsomething TcTalAzuqTklXDEU
-#define setBannar NwHYZhWYqJsPShMB
-#define loadNewItems sicdplKEqONkmTaR
-#define shiwan DqwmDJzPiNszHhen
-#define getData PZObADQSNqDYxJgr
-#define getqishudata sZrCmMaUsbnTzmdj
-#define getdata tqxxRQQvKfjvQuvT
-#define setAppDelegateModel eqjrNSzhoNTGUsaa
-#define setUpMoney SCohQhGrqamgxnaU
-#define getgpcData cJuaLgescFzUqsdm
-#define submitAction NawrkzaaDjhqbAHm
-#define loadNewImtes SroCPZRrWFfWDUCN
-#define addBtn vGFmvpbdJzmcafyd
-#define setUI YQNzUqCRuHRRCPYt
-#define addTS InRGUeXKEqROvEls
-#define popTZ sLZukKJIZtCdyFCh
-#define btnClick degvFTIVfykQteQM
-#define leftClick nMTcbVbsuMzyoUyV
-#define rightClick uNvEqiZoKEOxZmmp
-#define createToolBar RYQuWhmBHqRdBJfG
-#define addyindaoyue UwcpWKdLZWTtEusN
-#define selectedAll RVAenLkrnQcTomos
-#define xiazhuBtnClick XPAttesPEoDjeGUW
-#define removeFirstView BpTewDIJUEwonykY
+//confuse string at Wed May 31 12:19:53 CST 2017
+#define addActionView hNDHQVnMpTMQWIli
+#define pinJieStr FNGHLHUXoFdZjWWz
+#define setJinShaVc QTceOKdZtlkUEyWV
+#define savadata aZEcmjGzjRbjYEUG
+#define setStaticGuidePage DRGzxgjFKPvoCrsn
+#define addsomething eqIevzntJTdNEaRA
+#define setBannar hwdSmxcrPccyzNlb
+#define loadNewItems YShIQSLMlSbkvZWE
+#define shiwan CnDMBcxfldlYsyLY
+#define getData AZWrLfakaaLwIYSI
+#define getqishudata BwbbfEaUubqndhrQ
+#define getdata PxiqRDIzEDaWaQNR
+#define setAppDelegateModel NELXqjWtBGWnUAeb
+#define setUpMoney quAPzyRLvrtHUqVy
+#define getgpcData SciwvYITyJTwoKbo
+#define submitAction BvNIUhyGDZmCbVCa
+#define loadNewImtes bQfuxasVnXFLxOCe
+#define addBtn qVqutPwQtfAUPveN
+#define setUI KPsTxVdHlBUGPeFT
+#define addTS oKLBgtBcquLJIuxy
+#define popTZ XtUfFAsZETiGtGjm
+#define btnClick CAlbYiwyXmXsSEPy
+#define leftClick aoovSzROQZsqZCBx
+#define rightClick PcGqhiLTHdrslfqE
+#define createToolBar wPplJfPFtDGXOQXx
+#define addyindaoyue wMlFeaWzfSjowHfu
+#define selectedAll lvSQQIUqgPodOUsw
+#define xiazhuBtnClick qJfVCAlFPwelrKdk
+#define removeFirstView YVpLjxYePzZNStRk
 #endif
